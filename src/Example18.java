09698f7eb636710bbe1a28db96404315bfd788a6
Bianca Milea <alumil2013@ieselcaminas.org>
2022-10-11 23:17:46 +0200

    change Example18

--- src/Example18.java
+++ src/Example18.java
@@ -9,25 +9,24 @@ public class Example18 {
         double taxes = 0;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce the price per hour: ");
-        pricePerHour = scanner.nextDouble();
         System.out.println("Introduce the number of hours: ");
         hours = scanner.nextDouble();
+        System.out.println("Introduce the price per hour: ");
+        pricePerHour = scanner.nextDouble();
+
 
         //calculate the gross pay
-
-
         if(hours < 35){
             grossPay = hours * pricePerHour;
         }else{
-            grossPay = (35 * pricePerHour) + ((hours - 35) * (pricePerHour * 1.5));
+            grossPay = 35 * pricePerHour + (hours - 35) * pricePerHour * 1.5;
         }
 
         //calculate taxes
         if(grossPay > 500 && grossPay <= 900){
             taxes = (grossPay - 500) * 0.25;
         }else if(grossPay > 900){
-            taxes = (grossPay - 500) * 0.45;
+            taxes = 400 * 0.25 + (grossPay - 900) * 0.45;
         }else{
             taxes = 0;
         }
@@ -35,7 +34,9 @@ public class Example18 {
         //calculate net pay
         netPay = grossPay - taxes;
 
-        System.out.println("The net pay is: " + netPay);
+        System.out.println("Gross salary = " + grossPay);
+        System.out.println("Taxes = " + taxes);
+        System.out.println("Net pay = " + netPay);
     }
 
 
